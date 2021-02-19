06db4c6cfa4831ed920e60763d941c52784d8170
iZarif <aydar.js@gmail.com>
2021-02-19 12:23:57 +0300

    format code

--- hello.go
+++ hello.go
@@ -4,84 +4,84 @@ import "github.com/Shopify/go-lua"
 import "math"
 
 type item_t struct {
-  x int
-  y int
-  address string
+	x       int
+	y       int
+	address string
 }
 
 func l_sin(l *lua.State) int {
-  l.PushNumber(math.Sin(lua.CheckNumber(l, 1)))
+	l.PushNumber(math.Sin(lua.CheckNumber(l, 1)))
 
-  return 1
+	return 1
 }
 
 func l_isItem(l *lua.State) int {
-  item := lua.TestUserData(l, 1, "my.item")
+	item := lua.TestUserData(l, 1, "my.item")
 
-  if (item != nil) {
-    l.PushBoolean(true)
-  } else {
-    l.PushBoolean(false)
-  }
+	if item != nil {
+		l.PushBoolean(true)
+	} else {
+		l.PushBoolean(false)
+	}
 
-  return 1
+	return 1
 }
 
 func l_pushItem(l *lua.State, item *item_t) {
-  l.PushUserData(item)
-  lua.SetMetaTableNamed(l, "my.item")
+	l.PushUserData(item)
+	lua.SetMetaTableNamed(l, "my.item")
 }
 
 func l_sqlSflow(l *lua.State) int {
-  //statement := lua.CheckString(l, 1)
-  // items := sql_sflow(statement)
-  items := []item_t{{0, 0, "add1"}, {0, 1, "add2"}, {0, 3, "add3"}}
-  l.NewTable()
+	//statement := lua.CheckString(l, 1)
+	// items := sql_sflow(statement)
+	items := []item_t{{0, 0, "add1"}, {0, 1, "add2"}, {0, 3, "add3"}}
+	l.NewTable()
 
-  for i := 0; i < len(items); i++ {
-    l.PushInteger(i + 1)
-    l_pushItem(l, &items[i])
-    l.RawSet(-3)
-  }
+	for i := 0; i < len(items); i++ {
+		l.PushInteger(i + 1)
+		l_pushItem(l, &items[i])
+		l.RawSet(-3)
+	}
 
-  return 1
+	return 1
 }
 
 func l_indexItem(l *lua.State) int {
-  item := l.ToUserData(1).(*item_t)
-  key, _ := l.ToString(2)
+	item := l.ToUserData(1).(*item_t)
+	key, _ := l.ToString(2)
 
-  switch key {
-    case "address":
-      l.PushString(item.address)
-    default:
-      l.PushNil()
-  }
+	switch key {
+	case "address":
+		l.PushString(item.address)
+	default:
+		l.PushNil()
+	}
 
-  return 1
+	return 1
 }
 
 func main() {
-  l := lua.NewState()
-  lua.OpenLibraries(l)
+	l := lua.NewState()
+	lua.OpenLibraries(l)
 
-  itemMetaFuncs := []lua.RegistryFunction{
-    {"__index", l_indexItem},
-  }
+	itemMetaFuncs := []lua.RegistryFunction{
+		{"__index", l_indexItem},
+	}
 
-  lua.NewMetaTable(l, "my.item")
-  lua.SetFunctions(l, itemMetaFuncs, 0)
+	lua.NewMetaTable(l, "my.item")
+	lua.SetFunctions(l, itemMetaFuncs, 0)
 
-  myLibrary := []lua.RegistryFunction{
-    {"sin", l_sin},
-    {"sqlSflow", l_sqlSflow},
-    {"isItem", l_isItem},
-  }
+	myLibrary := []lua.RegistryFunction{
+		{"sin", l_sin},
+		{"sqlSflow", l_sqlSflow},
+		{"isItem", l_isItem},
+	}
 
-  lua.NewLibrary(l, myLibrary)
-  l.SetGlobal("my")
+	lua.NewLibrary(l, myLibrary)
+	l.SetGlobal("my")
 
-  if err := lua.DoFile(l, "hello.lua"); err != nil {
-    panic(err)
-  }
+	if err := lua.DoFile(l, "hello.lua"); err != nil {
+		panic(err)
+	}
 }
